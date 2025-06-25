8fd6bc6d93a7619473cee27c246921d608fd296d
code <code@mac>
2025-06-25 22:50:43 +0700

    add base path for vite build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 /** @type {import('vite').UserConfig} */
 export default {
+  base: "./",
   server: {
     port: 3000,
     strictPort: true,
-    open: "demo.html",
+    open: "index.html",
     middlewareMode: false,
     https: {
       key: "../../js/localhost.key",
