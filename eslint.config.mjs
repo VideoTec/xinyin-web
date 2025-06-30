d383b323d0f2df89f1cc24b63edf31014a60f774
code <code@mac>
2025-06-30 14:41:13 +0700

    add eslint-plugin-react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,21 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
+import react from "eslint-plugin-react";
 import globals from "globals";
 import { globalIgnores } from "eslint/config";
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
   {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     languageOptions: {
       globals: {
         ...globals.browser,
