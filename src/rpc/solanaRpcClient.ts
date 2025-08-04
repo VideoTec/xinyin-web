e422ba52486b7f0ac873f9317604a1c5368bb782
code <code@mac>
2025-08-04 18:02:05 +0700

    change solana rpc url

--- src/rpc/solanaRpcClient.ts
+++ src/rpc/solanaRpcClient.ts
@@ -49,7 +49,7 @@ export async function callSolanaRpc<T>(
 
   const url =
     currentCluster === "mainnet-beta"
-      ? "https://dry-silence-6b88.wangxiangc.workers.dev/"
+      ? "https://solana.wangxiang.work"
       : currentCluster === "testnet"
       ? "https://api.testnet.solana.com"
       : "https://api.devnet.solana.com";
